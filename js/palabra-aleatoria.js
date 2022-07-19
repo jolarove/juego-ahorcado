37a44bb7c9ee1435bfa86743f5968d39e5d17a03
Lauro Rodríguez <laurorodven@gmail.com>
2022-07-19 08:38:06 -0500

    Adicionando función para elegir palabra y letras

--- js/palabra-aleatoria.js
+++ js/palabra-aleatoria.js
@@ -4,18 +4,41 @@ console.log(numeroAleatorio);
 var palabraAleatoria = palabras[numeroAleatorio];
 console.log(palabraAleatoria);
 console.log(palabraAleatoria[1]);
+var i = 0;
+var posicion1 = document.querySelector("#posicion-uno");
+posicion1.textContent = palabraAleatoria[0];
+i++;
 
-function elegirLetra(i){
-    var i=0;
-    console.log(palabraAleatoria[i]);
-}
 
-function crearP(i){
+function construirP(dato, clase){
     var p = document.createElement("p");
-    p.classList.add(".letra");
-    p.textContent = elegirLetra(i)
+    p.textContent = dato;
+    p.classList.add(clase);
+    return p;
 }
 
-elegirLetra;
+function construirLi (letra, clase){
+    //Crear item de lista
+    var letraLi = document.createElement("li");
+      
+    //Agregar nuevos valores al nuevo item
+    
+    letraLi.appendChild(construirP(letra, clase));
+
+    return letraLi;
+}
+
+var lista = document.querySelector(".letras")
+
+function crearItem(){
+    while (i < palabraAleatoria.length) {
+        lista.appendChild(construirLi(palabraAleatoria[i], "letra"));
+        lista.classList.add("letras");
+        i++ 
+    }
+}
+
+crearItem();
+
 
 
